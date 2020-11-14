4032c4e97e3afd551f4cfcdc675b9acf2a928121
Andy Ngo <andyngo2021@gmail.com>
2020-11-14 11:41:08 -0800

    added youtube api

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # defhacks2-voice-assistant
 
 For the 2020 DefHacks Hackathon 11/14-11/15 :D
+
+Running on Python 3.8.5 64-bitbit
---
+++ YTSearch.py
@@ -0,0 +1,27 @@
+from googleapiclient.discovery import build
+import webbrowser
+
+# Using this class to communicate with YouTube or doing anything relating to YouTube
+
+class YouTubeAPI:
+    def __init__(self):
+        self.youtube = build('youtube', 'v3', developerKey=self.getAPIKey())
+    
+    def getAPIKey(self):
+        with open('youtubeapikey.txt') as keydata:
+            data = keydata.readlines()
+            api_key = data[0].strip()
+            return str(api_key)
+
+    def SearchForVideo(self, title, author):
+        request = self.youtube.search().list(
+            part="snippet",
+            maxResults=1,
+            q=f"{title} {author}"
+        )
+        response = request.execute()
+        id = response["items"][0]["id"]["videoId"]
+        return id
+
+    def GetYouTubeURL(self, id):
+        return f'https://youtu.be/{id}'
